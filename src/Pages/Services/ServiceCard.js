ebbd4454e3ff43f90afe36cf6f829e43e0f5590b
SheikhMujib <mbasheikhmujib@gmail.com>
2022-11-08 12:59:46 +0600

    Take some Rest

--- src/Pages/Services/ServiceCard.js
+++ src/Pages/Services/ServiceCard.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const ServiceCard = ({service}) => {
-  const {img, title, price} = service;
+const ServiceCard = ({ service }) => {
+  const {_id, img, title, price } = service;
   return (
     <div className="card card-compact w-96 bg-base-100 shadow-xl">
       <figure>
@@ -9,9 +10,13 @@ const ServiceCard = ({service}) => {
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
-        <p className="text-2xl font-semibold text-orange-600">Price: ${price}</p>
+        <p className="text-2xl font-semibold text-orange-600">
+          Price: ${price}
+        </p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <Link to={`/checkout/${_id}`}>
+            <button className="btn btn-primary">Buy Now</button>
+          </Link>
         </div>
       </div>
     </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import logo  from '../../../assets/logo.svg'
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Header = () => {
+  const {user} = useContext(AuthContext);
     const menuItems = <>
     <li>
         <Link to='/' className='font-semibold'>Home</Link>
     </li>
-    <li>
-        <Link to='/login' className='font-semibold'>Login</Link>
-    </li>
+    {
+      user?.email ?
+      <>
+      <li>
+        <Link to='/orders' className='font-semibold'>Orders</Link>
+      </li>
+      </>
+      :
+      <li>
+      <Link to='/login' className='font-semibold'>Login</Link>
+  </li>
+    }
     </>
     return (
         <div className="navbar h-20 mb-12 pt-12 bg-base-100">
